556ec5403c380964f065424e81a0cce2a6d70248
Ankita Rathod <rathodankita9602@gmail.com>
2025-09-10 22:22:23 +0530

    added problems on filter and map

--- map,reduce,filter.js
+++ map,reduce,filter.js
@@ -27,6 +27,8 @@ console.log(str1)
 
 
 //---------------------------------
+//Extract Names
+
  let users = [
 {
     name : "Alice",
@@ -47,3 +49,43 @@ console.log(names)
 }
 
 objname();
+//---------------------------------
+
+//Square Each Number
+
+let h1 = [7,26,51]
+const test = h1.map((e)=> e*e)
+console.log(test)
+
+
+///--------------Filter------------
+
+//Even numbers only 
+let j1=[1, 2, 3, 4, 5, 6];
+const even = j1.filter((e)=> e%2===0)
+console.log(even)
+
+//---------------------------
+//Words Longer than 4 Letters
+
+let str8= ["cat", "lion", "tiger", "dog","peacock", "Zeb raa"];
+str8.filter((e)=> {
+  if(e.length>4){
+      console.log(e)
+  }
+})
+
+//---------------------------
+//Filter Adults
+
+let persons = [{name:'Noah', age : 19},
+    {name:'Tesa' , age : 7},
+    {name:'Jay' , age : 18},
+    {name:'Groot' , age : 15},
+]
+
+persons.filter((e)=>{
+    if (e.age >= 18){
+        console.log(e.name)
+    }
+})
